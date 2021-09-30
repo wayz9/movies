ff8072b93285f4e89ae16f432c36f03e7beb6413
Not Available <ventusblade1@gmail.com>
2021-09-30 23:53:55 +0200

    wip

--- app/View/Components/AdminLayout.php
+++ app/View/Components/AdminLayout.php
@@ -2,12 +2,21 @@
 
 namespace App\View\Components;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\Component;
 
 class AdminLayout extends Component
 {
     public function render()
     {
-        return view('layouts.admin');
+        $adminMovieGenres = Cache::remember('genres_movies', now()->addMonths(6), function () {
+            return tmdb('genre/movie/list', 'genres')->all();
+        });
+
+        $adminTvGenres = Cache::remember('genres_tv', now()->addMonths(6), function () {
+            return tmdb('genre/tv/list', 'genres')->all();
+        });
+
+        return view('layouts.admin', compact('adminMovieGenres', 'adminTvGenres'));
     }
 }
